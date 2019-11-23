2ff0b6123fc30c1bb17c036aeb518510a18f3014
ahmednafies <ahmed.nafies@gmail.com>
2019-11-23 21:10:46 +0100

    adds urls

---
+++ api/urls.py
@@ -0,0 +1,49 @@
+from django.urls import path
+from api.views import (
+    GetMediaByUUID,
+    ListMedia,
+    AddMetaFields,
+    DeleteMetaFields,
+)
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Media Asset Manager API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
+    path("media/", ListMedia.as_view(), name="media-list"),
+    path("media/<str:uuid>", GetMediaByUUID.as_view(), name="media-detail"),
+    path(
+        "media/<str:uuid>/meta-fields/create-multiple",
+        AddMetaFields.as_view(),
+        name="meta-fields-create-multiple",
+    ),
+    path(
+        "media/<str:uuid>/meta-fields/delete-multiple",
+        DeleteMetaFields.as_view(),
+        name="meta-fields-delete-multiple",
+    ),
+]
