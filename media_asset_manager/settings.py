2d1200ddcfa2e15722fa6408825943408ca73806
ahmednafies <ahmed.nafies@gmail.com>
2019-11-23 22:45:31 +0100

    removes camel casing to simplify tests

--- media_asset_manager/settings.py
+++ media_asset_manager/settings.py
@@ -60,18 +60,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-REST_FRAMEWORK = {
-    "DEFAULT_RENDERER_CLASSES": (
-        "djangorestframework_camel_case.render.CamelCaseJSONRenderer",
-        "djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer",
-    ),
-    "DEFAULT_PARSER_CLASSES": (
-        "djangorestframework_camel_case.parser.CamelCaseFormParser",
-        "djangorestframework_camel_case.parser.CamelCaseMultiPartParser",
-        "djangorestframework_camel_case.parser.CamelCaseJSONParser",
-    ),
-    "JSON_UNDERSCOREIZE": {"no_underscore_before_number": True},
-}
 
 ELASTICSEARCH_DSL = {"default": {"hosts": "localhost:9200"}}
 
