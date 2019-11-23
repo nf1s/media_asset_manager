edfad73489095c3f7f165233689df3a1926fe13b
ahmednafies <ahmed.nafies@gmail.com>
2019-11-23 21:11:57 +0100

    adds views

---
+++ api/views.py
@@ -0,0 +1,65 @@
+from api import serializers
+from rest_framework import generics
+from app.models import Media
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
+
+
+class GetMediaByUUID(generics.RetrieveAPIView):
+
+    serializer_class = serializers.MediaModelSerializer
+
+    lookup_field = "uuid"
+    queryset = Media.objects.all()
+
+
+class ListMedia(generics.ListAPIView):
+
+    serializer_class = serializers.MediaModelSerializer
+    queryset = Media.objects.all()
+
+
+class AddMetaFields(APIView):
+    @swagger_auto_schema(
+        request_body=serializers.AddMultipleMetaFieldsSerializer(),
+        operation_id="meta_fields_update",
+        responses={
+            status.HTTP_200_OK: serializers.AddMultipleMetaFieldsSerializer
+        },
+    )
+    def put(self, request: Request, **kwargs):
+        serializer = serializers.AddMultipleMetaFieldsSerializer(
+            data=request.data
+        )
+        if not serializer.is_valid():
+            pass
+
+        serializer.add_multiple(kwargs["uuid"], request.data["meta_fields"])
+        return Response(data=serializer.data)
+
+
+class DeleteMetaFields(APIView):
+    @swagger_auto_schema(
+        request_body=serializers.DeleteMultipleMetaFieldsSerializer(),
+        operation_id="meta_fields_delete",
+        responses={
+            status.HTTP_200_OK: serializers.DeleteMultipleMetaFieldsSerializer
+        },
+    )
+    def post(self, request: Request, **kwargs):
+        serializer = serializers.DeleteMultipleMetaFieldsSerializer(
+            data=request.data
+        )
+        if serializer.is_valid():
+            serializer.delete_multiple(
+                kwargs["uuid"], request.data["meta_field_names"]
+            )
+            return Response(data=serializer.data)
+
+        return Response(
+            status=status.HTTP_400_BAD_REQUEST,
+            data={"errors": serializer.errors},
+        )
