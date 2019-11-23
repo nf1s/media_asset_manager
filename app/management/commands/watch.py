8bc36637f1c080dbcead87d9c923921f1653d5ca
ahmednafies <ahmed.nafies@gmail.com>
2019-11-23 16:01:24 +0100

    adds django cmd to watch dir

---
+++ app/management/commands/watch.py
@@ -0,0 +1,69 @@
+import time
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
+from app.models import Media
+
+
+class Command(BaseCommand):
+    help = "Watches directory specified in settings.py"
+
+    def handle(self, *args, **options):
+        path = settings.WATCH_DIR
+        observer = Observer()
+        observer.schedule(WatchDogHandler(), path=path)
+        observer.start()
+        self.stdout.write(self.style.SUCCESS(f"Watching {path} ..."))
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+
+        observer.join()
+
+        self.stdout.write(self.style.SUCCESS("Successfully read files"))
+
+
+class WatchDogHandler(PatternMatchingEventHandler):
+    patterns = ["*.txt"]
+
+    def process(self, event):
+        """
+        event.event_type
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        event.src_path
+            path/to/observed/file
+        """
+
+        with open(event.src_path, "r"):
+            pass
+
+    def _get_name_from_path(self, path):
+        return path.split("/")[-1]
+
+    def on_created(self, event):
+        name = self._get_name_from_path(event.src_path)
+        Media.objects.create(name=name)
+        print(f"{event.src_path} is {event.event_type}")
+
+    def on_modified(self, event):
+        print(f"{event.src_path} is {event.event_type}")
+
+    def on_deleted(self, event):
+        name = self._get_name_from_path(event.src_path)
+        Media.objects.delete(name=name)
+        print(f"{event.src_path} is {event.event_type}")
+
+    def on_moved(self, event):
+        # TODO:check if the file is moved to the same directory
+        name = self._get_name_from_path(event.src_path)
+        new_name = self._get_name_from_path(event.dest_path)
+        media = Media.objects.get(name=name)
+        media.name = new_name
+        media.save()
+        print(f"{event.src_path} is {event.event_type} to {event.dest_path}")
