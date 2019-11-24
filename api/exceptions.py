b65514da56f913e6aeffe35c806d5105f1b0af79
ahmednafies <ahmed.nafies@gmail.com>
2019-11-24 12:21:03 +0100

    cleans api

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """API Exceptions Module
     All exceptions related to API endpoints are handeld here
 """
@@ -5,6 +6,8 @@ from rest_framework.exceptions import APIException
 
 
 class DoesNotExist(APIException):
+    """ Handels errors When field does not exist in database"""
+
     status_code = 400
     default_detail = "Error: Does Not Exist"
     default_code = "Does Not exist"
