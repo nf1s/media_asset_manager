53500c514a7ec0c016b2d6773b936226fa800c07
ahmednafies <ahmed.nafies@gmail.com>
2019-11-24 12:21:19 +0100

    cleans api

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,17 @@
+# -*- coding: utf-8 -*-
+"""API Apps Module
+This Module contains the models used for serializing data
+for all endpoints
+"""
+
 from rest_framework import serializers
 from app.models import Media, MetaFields
 from api import exceptions
 
 
 class MetaFieldModelSerializer(serializers.ModelSerializer):
+    """Serializes data for MetaFields Model"""
+
     class Meta:
         model = MetaFields
         exclude = ["id", "media"]
@@ -11,6 +19,8 @@ class MetaFieldModelSerializer(serializers.ModelSerializer):
 
 
 class MediaModelSerializer(serializers.ModelSerializer):
+    """Serializes data for Media Model and nests MetaFields"""
+
     meta_fields = MetaFieldModelSerializer(
         many=True, required=False, read_only=True
     )
@@ -22,9 +32,18 @@ class MediaModelSerializer(serializers.ModelSerializer):
 
 
 class AddMultipleMetaFieldsSerializer(serializers.Serializer):
+    """Serializer handles adding and/or updating multiple meta field values"""
+
     meta_fields = MetaFieldModelSerializer(many=True)
 
-    def add_update_multiple(self, uuid, fields):
+    def add_update_multiple(self, uuid: str, fields: dict):
+        """Method adds or updates multiple meta fields to
+        
+        Args:
+            uuid (str): media uuid
+            fields (list[dict]): List of dicts containing meta field values
+            e.g [{"name":"field_1", "value":"value_1"}]
+        """
         media = Media.objects.get(uuid=uuid)
         for field in fields:
             meta_field, _ = media.meta_fields.get_or_create(name=field["name"])
@@ -33,12 +52,22 @@ class AddMultipleMetaFieldsSerializer(serializers.Serializer):
 
 
 class DeleteMultipleMetaFieldsSerializer(serializers.Serializer):
+    """Serializer deletes mutiple meta fields by their keys"""
 
     meta_field_names = serializers.ListField(
         child=serializers.CharField(max_length=255), required=True
     )
 
-    def delete_multiple(self, uuid, field_names):
+    def delete_multiple(self, uuid: str, field_names: list):
+        """Method deletes multiple meta field by their keys or
+        
+        Args:
+            uuid (str): media uuid
+            field_names (list[str]): List of field names to be deleted
+        
+        Raises:
+            exceptions.DoesNotExist: if there are field names that do not exit in database
+        """
         media = Media.objects.get(uuid=uuid)
         wrong_field_names = []
         for name in field_names:
